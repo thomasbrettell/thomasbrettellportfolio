4b436d3fdcbec859c1ba8a7959875dc770966d04
thomasbrettell <70102377+thomasbrettell@users.noreply.github.com>
2021-01-15 01:43:00 +1100

    cant remeber. finalised v1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// var tooltip = new bootstrap.Tooltip($('.turned-off'), top);
-
+// Bootstrap tooltips
 $(function () {
- $('[data-tooltip]').tooltip();
+    $('[data-tooltip]').tooltip();
 });
 
 // Variables
@@ -48,7 +47,61 @@ $(window).scroll(function () {
     }
 });
 
+// Intro btn functionality
+$($(".intro-btn")[0]).click(function () {
+    $(document).scrollTop($("#contact")[0].offsetTop - 64);
+});
+
+$(".cv-btn").click(function () {
+    window.open("assets/ThomasBrettellCV 9121.pdf");
+});
+
 // card button functionality
 $($(".carbon-quiz-btn")[1]).click(function () {
     window.open("https://thomasbrettell.itch.io/the-carbon-quiz-webgl");
-})
+});
+
+$($(".cazna-btn")[1]).click(function () {
+    window.open("https://thomasbrettell.github.io/cazna-site/");
+});
+
+$($(".sleep-orbit-btn")[1]).click(function () {
+    window.open("https://www.youtube.com/watch?v=kIWMUkRbbkg&ab_channel=ThomasBrettell");
+});
+
+$($(".global-death-btn")[1]).click(function () {
+    window.open("https://thomasbrettell.github.io/nationdeathsdatavis/");
+});
+
+$($(".olympics-vis-btn")[1]).click(function () {
+    window.open("assets/olympics-vis/Success-at-the-summer-Olympics-became-more-globalised-as-a-result-of-World-War-2.pdf");
+});
+
+$($(".domestic-violence-vis-btn")[1]).click(function () {
+    window.open("assets/domestic-violence-vis/Is-Summer-the-Season-for-Domestic-Violence.pdf");
+});
+
+$($(".museum-btn")[1]).click(function () {
+    window.open("assets/museum/Children-at-the-Museum.pdf");
+});
+
+// Navbar functionality
+$(".menu-links").click(function (e) {
+    switch (e.target.innerText) {
+        case "THOMAS BRETTELL":
+            $(document).scrollTop(0);
+            break;
+
+        case "My Work":
+            $(document).scrollTop($("#work")[0].offsetTop - 64);
+            break;
+
+        case "About Me":
+            $(document).scrollTop($("#about")[0].offsetTop - 64);
+            break;
+
+        case "Contact Me":
+            $(document).scrollTop($("#contact")[0].offsetTop - 64);
+            break;
+    }
+});
